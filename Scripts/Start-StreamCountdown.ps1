de3fb60352c9b12321b357201174cab5f2604ea6
Matthew DeGarmo <matthewjdegarmo@gmail.com>
2021-01-01 00:16:20 -0600

    Finishing function.

--- Scripts/Start-StreamCountdown.ps1
+++ Scripts/Start-StreamCountdown.ps1
@@ -1,4 +1,60 @@
+<#
+.SYNOPSIS
+    Create a countdown timer using a text file for Streamlabs OBS
+.PARAMETER StartTime
+    Provide the datetime formatted time for when the stream will start
+    ie. 3:45pm, 15:45, 8:00am, 8:00, etc..
+.PARAMETER MinutesUntilStart
+    Provide the number of minutes from now to start the stream.
+.EXAMPLE
+    PS> .\Start-StreamCountdown.ps1 -StartTime 08:10:34
+
+    Description
+    -----------
+    This will start a countdown, for 8:10:34 AM and create a countdown.txt file
+.NOTES
+    Author: Matthew J. DeGarmo
+    Blog: matthewjdegarmo.com
+#>
+
 [CmdletBinding()]
 Param(
-    [System.DateTime] $StartTime
+    [Parameter(Mandatory, ParameterSetName = 'Time')]
+    [System.DateTime] $StartTime,
+
+    [Parameter(Mandatory, ParameterSetName = 'Ad-Hoc')]
+    [System.String] $MinutesUntilStart
 )
+
+Begin {
+    $CountdownFilePath = Join-Path $PSScriptRoot 'Countdown.txt'
+    If (-Not(Test-Path $CountdownFilePath)) {
+        $null = New-Item -Path $CountdownFilePath -ItemType File -Force
+    }
+}
+
+Process {
+    Try {
+        $Now = Get-Date
+        Switch ($PSCmdlet.ParameterSetName) {
+            'Time' {
+                $DestinationTime = $StartTime
+            }
+            'Ad-Hoc' {
+                $DestinationTime = $Now.AddMinutes($MinutesUntilStart)
+            }
+        }
+        While ($DestinationTime -gt $Now) {
+            #Magic mod for seconds conversion to minutes
+            #which will output to file.
+            $Now = Get-Date
+            $RemainingTime = (New-TimeSpan -Start $Now -End $DestinationTime).ToString().Split('.')[0]
+            $RemainingTime | Out-File -FilePath $CountdownFilePath -Force
+            Start-Sleep -Seconds 1
+        }
+    } Catch {
+        Write-Error $_
+    } Finally {
+        $null = Remove-Item $CountdownFilePath -Force
+    }
+}
